fa1505431e8bd8b2d821c87867cde457d5a44fd7
Felipe Coimbra <137563995+felipecoimbra123@users.noreply.github.com>
2023-11-20 18:05:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const divPalavras = document.querySelectorAll('.palavrasEng h2')
 const abaPalavras = document.querySelector('.modeloPalavra')
 const fundoSite = document.querySelector('.fundoDicionario')
+const abaDicionario = document.querySelector('.abrirAba')
+const dicionario = document.querySelector('.exemploDialogo')
 
 
 for(let posicao = 0; posicao < divPalavras.length; posicao++) {
@@ -9,3 +11,21 @@ for(let posicao = 0; posicao < divPalavras.length; posicao++) {
         fundoSite.style.display = "flex"
     })
 }
+
+fundoSite.addEventListener('click', (e) => {
+    abaPalavras.style.display = "none"
+    fundoSite.style.display = "none"
+}
+)
+
+let abertoDicionario = true
+    
+abaDicionario.addEventListener('click', (e) => {
+    if (abertoDicionario) {
+        dicionario.style.display = "none"
+        abertoDicionario = false
+    } else {
+        dicionario.style.display = "flex"
+        abertoDicionario = true
+    }
+})
