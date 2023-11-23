1322cddf72e19c21eea959c3ff4364ade7193765
Felipe Coimbra <137563995+felipecoimbra123@users.noreply.github.com>
2023-11-23 18:11:17 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,6 +9,45 @@ for(let posicao = 0; posicao < palavrasEng.length; posicao++) {
     palavrasEng[posicao].addEventListener('click', (e) => {
         abaPalavras.style.display = "block"
         fundoSite.style.display = "flex"
+
+        const palavrasIngles = document.querySelector('.palavraIngles')
+        const palavrasPortugues = document.querySelector('.palavraPortugues')
+        const explicacaoIngles = document.querySelector('.explicacaoIngles')
+        const explicacaoPortugues = document.querySelector('.explicacaoPortugues')
+
+        if (e.target.textContent == "Behavior") {
+            palavrasPortugues.textContent = "Comportamento"
+            explicacaoIngles.textContent = "Government of a king, an emperor, a sovereign prince. Or a government exercised by any authority."
+            explicacaoPortugues.textContent = "Governo de um rei, de um imperador, de um príncipe soberano. Ou um governo exercido por uma autoridade qualquer."
+        } 
+        else if (e.target.textContent == "Happiness") {
+            palavrasPortugues.textContent = "Felicidade"
+        }
+        else if(e.target.textContent == "Suddenly"){
+            palavrasPortugues.textContent = "De repente"
+        }
+        else if(e.target.textContent == "Been"){
+            palavrasPortugues.textContent = "Estive"
+        }
+        else if(e.target.textContent == "Wind"){
+            palavrasPortugues.textContent = "Vento"
+        }
+        else if(e.target.textContent == "Outside"){
+            palavrasPortugues.textContent = "Fora"
+        }
+        else if(e.target.textContent == "Reign"){
+            palavrasPortugues.textContent = "Reinado"
+        }
+        else if(e.target.textContent == "Had"){
+            palavrasPortugues.textContent = "Tive"
+        }
+        else if(e.target.textContent == "Portrait"){
+            palavrasPortugues.textContent = "Retrato"
+        }
+        else if(e.target.textContent == "Proprety"){
+            palavrasPortugues.textContent = "Propriedade"
+        }
+        palavrasIngles.textContent = e.target.textContent
     })
 }
 
